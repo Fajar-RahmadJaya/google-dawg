3443cad11fa781382011e847f0585a9e60bd1cc1
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2025-11-24 18:04:04 +0700

    Add result tree view
    - Add funtion to open treeview

--- component/cli_component.py
+++ component/cli_component.py
@@ -1,6 +1,8 @@
 from utility.constant import (config)
+from ui.tree import Tree
 
 import json
+from PySide6.QtWidgets import QApplication
 
 
 class CLIComponent:
@@ -11,3 +13,11 @@ class CLIComponent:
             print("google-search path saved to config. You can change it on google-dawg/data/config.json.") # noqa
         except Exception as e:
             print(f"Failed to write google-search path: {e}")
+
+    def open_tree(self, site):
+        tree = QApplication.instance()
+        if not tree:
+            tree = QApplication([])
+        window = Tree(site)
+        window.show()
+        tree.exec()
